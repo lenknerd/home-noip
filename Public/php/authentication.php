e945deb9c3f6f79a3ff2d69284f4e84995cfc6a0
David Lenkner <lenknerd@gmail.com>
2016-02-21 21:41:49 -0500

    Working on authentication cookie set

--- Public/php/authentication.php
+++ Public/php/authentication.php
@@ -6,7 +6,63 @@
  */
 
 function validate($unm, $pwd) {
-	// Try getting user from database based
+	// Connect to database and declare a query
+	$conn = getDatabaseConnection();
+
+	// Prepare statement to check for users with that name
+	$statement = $conn->prepare('SELECT hashedpw
+		FROM users WHERE username = :username',
+		array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY) );
+	// Execute
+	$statement->execute(array(':username' => $unm));
+	$wps = $statement->fetchAll();
+
+	// A response that will be used below... let default be no-good
+	$rsp = JsonResponse_Str("Invalid username or password.");
+
+	// If no users available, send that err
+	if( count($wps) == 0 ) {
+		$rsp->respondAndExit();
+	}
+
+	// Get hashed password (could check that only one user exists, but okay)
+	$phash = $wps[0][0];
+	if( password_verify( $pwd, $phash ) ) {
+		$rsp->setSuccessful();
+		// If successful also create cred cookie
+		$cky = new CredCookie($unm, $phash);
+	}
+	$rsp->respondAndExit();
+}
+
+class CredCookie {
+	public $uname = "";
+	public $phash = "";
+
+	// What the cookie will be stored as on client
+	public static $cookiename = "homesite-credcookie";
+
+	// How long it should be stored for
+	public static $expSecs = 86400; // 1 day 
+
+	// Initiate from username and password hash (doesn't make cookie, just obj)
+	public function __construct($u, $ph) {
+		$this->uname = $u;
+		$this->phash = $ph
+	}
+
+	// Store on client side
+	public function saveToClient() {
+		// Make it the username appended onto the hash (60 chars, easy demarc)
+		$concatval = $this->phash . $this->uname;
+		setcookie( $this->cookiename,
+			$concatval,
+			$this->expSecs,
+			'/',
+			$_SERVER['HTTP_HOST'],
+			true ); // true for secure... ensure only happens over https
+	}
+
 }
 
 ?>
--- Public/php/utils.php
+++ Public/php/utils.php
@@ -1,8 +1,9 @@
 <?php
 /* utils.php
- * This contains various utilities non-specific to the site, but useful.  For
- * example, utility to decode JSON a bit more easily with greater flexibility.
- * Also, class for standard JSON response with my error handling.
+ * This contains various utilities non-specific to site content, used many
+ * places.  For example, utility to decode JSON more easily from JS side.
+ * Also, class for standard JSON response with my error handling.  Function
+ * to get database connection.
  * David Lenkner, c. 2016
  */
 
@@ -14,13 +15,19 @@ function json_decode_custom($json, $assoc = FALSE) {
 }
 
 /* Abstract class for response to a query, with success, err message, rest of
- * data will be specific to what purpose of request is */
+ * data will be specific to what purpose of request is
+ */
 abstract class JsonResponse {
 	// The success is a boolean, default false need to specifically set true
 	public $success = false;
 	
 	// The error message if any, to be filled in
 	public $errMessage = "Unspecified error.";
+
+	// Constructor default for an error message, just provide msg string
+	public function __construct($emsg) {
+		$this->errMessage = $emsg;
+	}
 
 	// Set no error
 	public function setSuccessful() {
@@ -29,12 +36,54 @@ abstract class JsonResponse {
 	}
 
 	// Each child needs to override this based on it's specific data
-	abstract public function assocArray();
+	abstract public function specificsAssocArray();
 
-	// Gets JSON for public items to be returned, echoes it
-	public function sendResponse() {
-		$rsp = (object) ['success' => $this->success, 'errMessage' => $this->msg];
+	// Gets JSON for public items to be returned, echoes it, stops
+	public function respondAndExit() {
+		$rsp = (object) ['success' => $this->success,
+			'errMessage' => $this->msg,
+			'specifics' => $this->specificsAssocArray()
+		];
+		echo json_encode( $rsp );
+		exit();
 	}
+}
+
+/* Simple instance of above abstract class, specific data is string with
+ * default empty string
+ */
+class JsonResponse_Str extends JsonResponse {
+	// Some string data that may be provided with response
+	public $specificString = "";
+
+	// The required override... no assoc array needed here, just a simple var
+	public function specificsAssocArray() {
+		return $specificString;
+	}
+
+}
+
+// Get database connection
+function getDatabaseConnection() {
+	$db_srvrname = "localhost";
+	$db_username = "noipsite";
+	$db_password = "noipsite";
+	$db_dbasname = "homesite";
+
+	// Create connection
+	$conn = null;
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$db_dbasname",
+			$username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		// On error, this returns success false
+		$eMsg = new JsonResponse_Str( $e->getMessage() );
+		$eMsg->respondAndExit();
+	}
+
+	return $conn
 }
 
 ?>
